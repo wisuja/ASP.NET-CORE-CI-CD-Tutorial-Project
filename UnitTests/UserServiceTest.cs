b59730265173342220e40e3fbf5ae299faaf21b8
williamgoh26 <william@partnermii.com>
2023-06-27 12:55:14 +0700

    Add project files.

---
+++ UnitTests/UserServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using WebApp.Models;
+using WebApp.Services;
+
+namespace UnitTests
+{
+    public class UserServiceTest
+    {
+        UserService userService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            userService = new UserService();
+        }
+
+        [Test]
+        public void FilterUserByFirstName_Works()
+        {
+            // Arrange
+            var name = "John";
+            var users = new List<User>()
+            {
+                new User() {FirstName = "John", LastName = "Doe"},
+                new User() {FirstName = "Jane", LastName = "Doe"},
+                new User() {FirstName = "Yui", LastName = "Kimura"},
+            };
+
+            // Act
+            var filteredUsers = userService.FilterUserByFirstName(name, users);
+
+            // Assert
+            Assert.That(filteredUsers, Is.EqualTo(users.Where(x => x.FirstName == name).ToList()));
+            Assert.That(filteredUsers, Is.Not.Null);
+        }
+    }
+}
---
+++ WebApp/Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿using WebApp.Models;
+
+namespace WebApp.Services
+{
+    public class UserService
+    {
+        public List<User> FilterUserByFirstName(string name, List<User> users)
+        {
+            return users.Where(x => x.FirstName == name).ToList();
+        }
+    }
+}
